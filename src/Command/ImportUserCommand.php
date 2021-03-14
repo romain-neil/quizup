42151e61bbf5306f19ccef3627bebfcca8ceb8d3
Romain Neil <tutosfaciles48@gmail.com>
2021-03-14 16:17:38 +0100

    Ajout commande d'importation des utilisateurs

---
+++ src/Command/ImportUserCommand.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Command;
+
+use App\Service\UserService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportUserCommand extends Command {
+
+	protected static $defaultName = 'app:import-user';
+
+	private UserService $userService;
+
+	public function __construct(UserService $userService, string $name = null) {
+		$this->userService = $userService;
+
+		parent::__construct($name);
+	}
+
+	public function configure() {
+		$this
+			->setDescription('Importation d\'utilisateurs')
+			->addArgument('file', InputArgument::REQUIRED)
+			->addArgument('nb', InputArgument::REQUIRED, 'Nombre d\'utilisateur à importer');
+	}
+
+	public function execute(InputInterface $input, OutputInterface $output): int {
+		$pBar = new ProgressBar($output);
+		$pBar->setMaxSteps($input->getArgument('nb'));
+
+		$this->userService->import(
+			$input->getArgument('file'),
+			$input->getArgument('nb'),
+			$pBar
+		);
+
+		$pBar->finish();
+
+		$output->writeln('<success>L\'import s\'est terminé sans problèmes</success>');
+
+		return Command::SUCCESS;
+	}
+
+}
---
+++ src/Service/UserService.php
@@ -0,0 +1,111 @@
+<?php
+namespace App\Service;
+
+use App\Entity\Classe;
+use App\Entity\Lycee;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\Reader\Xls;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Uid\Uuid;
+
+class UserService {
+
+	/**
+	 * @var EntityManagerInterface
+	 */
+	private EntityManagerInterface $manager;
+
+	public function __construct(EntityManagerInterface $manager) {
+		$this->manager = $manager;
+	}
+
+	public function import(string $file, int $nb, ProgressBar $pBar) {
+		$reader = new Xls();
+
+		$spreedsheet = $reader->load($file);
+		$spreedsheet->setActiveSheetIndex(0);
+
+		for($i = 0; $i < $nb; $i++) { //Pour chaque ligne
+			$page = $spreedsheet->getActiveSheet();
+
+			$user = new User();
+
+			$nomPrenom = $page->getCell("B" . $i)->getValue(); //DOE SPENCER John
+			$pass = $page->getCell("C" . $i)->getCalculatedValue();
+			$classe = $page->getCell("D" . $i)->getValue();
+
+			//trie du prénom et du nom
+			$nomPrenom = explode(" ", $nomPrenom);
+
+			$c = count($nomPrenom);
+			$c--;
+
+			$prenom = $nomPrenom[$c]; //Dernier element du tableau
+			$nom = [];
+
+			for($j = 0; $j < $c; $j++) {
+				$nom[] = $nomPrenom[$j];
+			}
+
+			//Récupération de l'établissement scolaire
+			$typeEPLE = $page->getCell("E" . $i)->getValue();
+			$nomEPLE = $page->getCell("F" . $i)->getValue();
+
+			//L'établissement n'a pas été enregistré en bdd
+			if(!isset($etablissements[$typeEPLE], $etablissements[$typeEPLE][$nomEPLE]["obj"])) {
+				$eple = new Lycee();
+
+				$eple->setType($typeEPLE);
+				$eple->setNom($nomEPLE);
+
+				$this->manager->persist($eple);
+				$this->manager->flush();
+
+				$etablissements[$typeEPLE][$nomEPLE]["obj"] = $eple;
+			}
+
+			if(!isset($etablissements[$typeEPLE][$nomEPLE]["classe"])) {
+				$ClasseEleve = new Classe();
+				$ClasseEleve->setLycee($etablissements[$typeEPLE][$nomEPLE]);
+
+				$etablissements[$typeEPLE][$nomEPLE]["classe"]["obj"] = $ClasseEleve;
+
+				/** @var Lycee $lycee */
+				$lycee = $etablissements[$typeEPLE][$nomEPLE]["obj"];
+				$lycee->addClass($ClasseEleve);
+
+				$this->manager->persist($lycee);
+
+				$etablissements[$typeEPLE][$nomEPLE]["obj"] = $lycee;
+			}
+
+			if(!isset($etablissements[$typeEPLE][$nomEPLE]["classe"]["prof_obj"])) {
+				$nomProf = $page->getCell("G" . $i); //TODO: fix me
+				$passProf = $page->getCell("H" . $i);
+
+				$prof = new User();
+				$prof->setNom($nomProf);
+				$prof->setPassword($passProf);
+				$prof->setRoles(["ROLE_PROF"]);
+
+				$this->manager->persist($prof);
+			}
+
+			//Le professeur n'a pas été enregistré en bdd
+
+			//Récupération du professeur
+
+			//Paramétrage de l'utilisateur
+			$user->setPrenom($prenom);
+			$user->setNom(implode(' ', $nom));
+			$user->setPassword($pass);
+			$user->setClasse($etablissements[$typeEPLE][$nomEPLE]["classe"]["obj"]);
+			$user->setUuid(Uuid::v4());
+
+			$this->manager->persist($user);
+
+			$pBar->advance();
+		}
+	}
+}
