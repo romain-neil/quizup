c4c59c16930143b9b612afec993e895c66ec5301
Romain Neil <tutosfaciles48@gmail.com>
2021-03-16 23:12:55 +0100

    Renommage du fichier

--- src/Command/ImportQuestionCommand.php
+++ src/Command/ImportQuestionCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ImportQuestion extends Command {
+class ImportQuestionCommand extends Command {
 
 	protected static $defaultName = 'app:import-question';
 
